88acac4639c7d8aa9f8591c4201779e9b11a1253
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-11-29 14:55:07 -0300

    classe de teste

---
+++ tdd/src/test/java/com/alga/tdd/util/CamelCaseConverterTest.java
@@ -0,0 +1,11 @@
+package com.alga.tdd.util;
+
+import org.junit.jupiter.api.*;
+
+public class CamelCaseConverterTest {
+
+    @Test
+    public void deveCriarObjetoCamelCaseConverter() throws Exception {
+        CamelCaseConverter = new CamelCaseConverter();
+    }
+}
