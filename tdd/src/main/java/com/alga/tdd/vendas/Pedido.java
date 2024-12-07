7c2abf3cfedb6e16da945abb057ecf60c4e021fd
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-07 20:28:02 -0300

    Seção 2.4 concluída

--- tdd/src/main/java/com/alga/tdd/vendas/Pedido.java
+++ tdd/src/main/java/com/alga/tdd/vendas/Pedido.java
@@ -3,9 +3,17 @@ package com.alga.tdd.vendas;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alga.tdd.vendas.desconto.CalculadoraFaixaDesconto;
+
 public class Pedido {
 
     private List<ItemPedido> itens = new ArrayList<>();
+
+    private CalculadoraFaixaDesconto calculadoraFaixaDesconto;
+
+    public Pedido(CalculadoraFaixaDesconto calculadoraFaixaDesconto) {
+        this.calculadoraFaixaDesconto = calculadoraFaixaDesconto;
+    }
 
     public void adicionarItem(ItemPedido itemPedido) {
         itens.add(itemPedido);
@@ -13,14 +21,14 @@ public class Pedido {
 
     public ResumoPedido resumo() {
         double valorTotal = itens.stream().mapToDouble(i -> i.getValorUnitario() * i.getQuantidade()).sum();
-        double desconto = 0;
-        if (valorTotal > 300.0 && valorTotal <= 800.00) {
-            desconto = valorTotal * 0.04;
-        } else if (valorTotal > 800.00 && valorTotal <= 1000.00) {
-            desconto = valorTotal * 0.06;
-        } else if (valorTotal > 1000.00) {
-            desconto = valorTotal * 0.08;
-        }
+        double desconto = calculadoraFaixaDesconto.desconto(valorTotal);
+        // if (valorTotal > 300.0 && valorTotal <= 800.00) {
+        // desconto = valorTotal * 0.04;
+        // } else if (valorTotal > 800.00 && valorTotal <= 1000.00) {
+        // desconto = valorTotal * 0.06;
+        // } else if (valorTotal > 1000.00) {
+        // desconto = valorTotal * 0.08;
+        // }
 
         return new ResumoPedido(valorTotal, desconto);
     }
