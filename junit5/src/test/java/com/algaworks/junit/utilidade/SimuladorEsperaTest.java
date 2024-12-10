bf47898377ab8d6a3cd020b994e85124a17a7199
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-10 19:55:42 -0300

    seção 2.6 finalizada

---
+++ junit5/src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -0,0 +1,26 @@
+package com.algaworks.junit.utilidade;
+
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+public class SimuladorEsperaTest {
+
+    @Test
+    void deveEsperarENaoDarTimeout() {
+        // esse aqui espera a função terminar totalmente mesmo passando da duração
+        // estipulada no teste
+        assertTimeout(Duration.ofSeconds(1), () -> {
+            SimuladorEspera.esperar(Duration.ofMillis(100));
+        });
+        // esse aqui fica esperando só o tempo determinado
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            SimuladorEspera.esperar(Duration.ofMillis(200));
+        });
+
+    }
+
+}
