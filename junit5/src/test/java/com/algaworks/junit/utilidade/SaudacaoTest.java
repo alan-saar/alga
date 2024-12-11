6c3c2cd4a13ecc62432d87027d1441831c873ffd
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-10 21:05:49 -0300

    debug

--- junit5/src/test/java/com/algaworks/junit/utilidade/SaudacaoTest.java
+++ junit5/src/test/java/com/algaworks/junit/utilidade/SaudacaoTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 
 public class SaudacaoTest {
 
-  @Test
-  public void saudar() {
-    String saudacao = SaudacaoUtil.saudar(9);
-    System.out.println(saudacao);
+    @Test
+    public void saudar() {
+        String saudacao = SaudacaoUtil.saudar(9);
+        System.out.println(saudacao);
 
-    assertEquals("Bom dia", saudacao, "Saudação incorreta");
-  }
+        assertEquals("Bom dia", saudacao, "Saudação incorreta");
+    }
 
-  @Test
-  public void deveLancarException() {
-    var exception = assertThrows(IllegalArgumentException.class, () -> {
-      SaudacaoUtil.saudar(-10);
-    });
+    @Test
+    public void deveLancarException() {
+        var exception = assertThrows(IllegalArgumentException.class, () -> {
+            SaudacaoUtil.saudar(-10);
+        });
 
-    assertEquals("Hora inválida", exception.getMessage(), "deve lançar a mensagem da exception corretamente");
+        assertEquals("Hora inválida", exception.getMessage(), "deve lançar a mensagem da exception corretamente");
 
-  }
+    }
 
-  public void naoDeveLancarException() {
-    assertDoesNotThrow(() -> {
-      SaudacaoUtil.saudar(0);
-    });
-  }
+    public void naoDeveLancarException() {
+        assertDoesNotThrow(() -> {
+            SaudacaoUtil.saudar(0);
+        });
+    }
 
 }
