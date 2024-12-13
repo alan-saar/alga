399e045a83ea21c515053cde7a229f1cb8d03e1b
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-13 14:58:45 -0300

    4,9 capturando paramentos com ArgumentCaptor

--- junit5/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockAnotationTest.java
+++ junit5/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockAnotationTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.*;
@@ -25,6 +27,9 @@ import com.algaworks.junit.blog.modelo.Editor;
 public class CadastroEditorComMockAnotationTest {
 
     Editor editor;
+
+    @Captor
+    ArgumentCaptor<Mensagem> mensagemArgumentCaptor;
 
     @Mock
     // a instância é renovada a cada novo teste
@@ -81,6 +86,28 @@ public class CadastroEditorComMockAnotationTest {
                 () -> assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
                 // verifica se não houve nenhuma chamada ao método de envio de email
                 () -> verify(gerenciadorEnvioEmail, never()).enviarEmail(any()));
+
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
+        // preciso pegar o que foi enviado para mensagem, porque é um método que retorna
+        // void
+
+        // @formatter:off
+        // Este mesmo argumentCaptor pode ser utilizado com anotacao @Captor
+        // ArgumentCaptor<Mensagem> mensagemArgumentCaptor = ArgumentCaptor.forClass(Mensagem.class);
+        // @formtatter:on
+
+        var editorSalvo = cadastroEditor.criar(editor);
+
+        verify(gerenciadorEnvioEmail).enviarEmail(mensagemArgumentCaptor.capture());
+
+        // mensagem passada quando o email foi enviado
+        Mensagem mensagem = mensagemArgumentCaptor.getValue();
+
+        // verifica se a mensagem passada é o mesmo do editor
+        assertEquals(editorSalvo.getEmail(), mensagem.getDestinatario());
 
     }
 
