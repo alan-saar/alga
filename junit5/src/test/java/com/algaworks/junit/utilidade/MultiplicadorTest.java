bf0662bd19ffe3eabe20e65ae9480976c3751201
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-14 11:30:23 -0300

    6.3 carregando valores de enum

---
+++ junit5/src/test/java/com/algaworks/junit/utilidade/MultiplicadorTest.java
@@ -0,0 +1,24 @@
+package com.algaworks.junit.utilidade;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+/**
+ * MultiplicadorTest
+ */
+public class MultiplicadorTest {
+
+    @ParameterizedTest
+    @EnumSource(value = Multiplicador.class, names = { "DOBRO", "TRIPLO" })
+    void aplicarMultiplicador(Multiplicador multiplicador) {
+        assertNotNull(multiplicador.aplicarMultiplicador(10.0));
+    }
+
+    @ParameterizedTest
+    @EnumSource(Multiplicador.class)
+    void aplicarMultiplicadorTodos(Multiplicador multiplicador) {
+        assertNotNull(multiplicador.aplicarMultiplicador(10.0));
+    }
+}
